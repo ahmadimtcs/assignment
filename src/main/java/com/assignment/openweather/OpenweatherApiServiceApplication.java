42f38d919c3f6781110cf10065e51ce0e904af57
Joseph Chandy <joseph.chandy@clairvoyantsoft.com>
2022-01-28 12:34:59 +0530

    initial commit

---
+++ src/main/java/com/assignment/openweather/OpenweatherApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.openweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenweatherApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenweatherApiServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assignment/openweather/OpenweatherApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.openweather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenweatherApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
